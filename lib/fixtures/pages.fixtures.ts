7d20afcba33ec13bb1822b63a0c77aa96da1692c
Butch Mayhew <bmayhew@tilled.com>
2025-04-18 15:31:58 -0500

    adding first page fixture

---
+++ lib/fixtures/pages.fixtures.ts
@@ -0,0 +1,12 @@
+import { LoginPage } from "@pages/login/login.page";
+import { test as baseTest } from "@playwright/test";
+
+type MyPages = {
+  loginPage: LoginPage;
+};
+
+export const test = baseTest.extend<MyPages>({
+  loginPage: async ({ page }, use) => {
+    await use(new LoginPage(page));
+  },
+});
