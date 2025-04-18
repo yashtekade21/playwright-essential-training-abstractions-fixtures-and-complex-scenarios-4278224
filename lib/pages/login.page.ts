986246666e5573077f5bd98f1c7c7f8e38f4af16
Butch Mayhew <bmayhew@tilled.com>
2025-04-18 09:23:08 -0500

    adding the login page object

---
+++ lib/pages/login.page.ts
@@ -0,0 +1,14 @@
+import { type Locator, type Page } from "@playwright/test";
+
+export class LoginPage {
+  readonly page: Page;
+  readonly emailInput: Locator;
+  readonly passwordInput: Locator;
+  readonly loginButton: Locator;
+  constructor(page: Page) {
+    this.page = page;
+    this.emailInput = page.getByTestId("email");
+    this.passwordInput = page.getByTestId("password");
+    this.loginButton = page.getByTestId("login-submit");
+  }
+}
