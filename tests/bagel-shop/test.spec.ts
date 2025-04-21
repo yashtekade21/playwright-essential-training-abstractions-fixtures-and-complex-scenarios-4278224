342355e47140ccd5baf2e2ef4ea9dc74588de3e0
Butch Mayhew <bmayhew@tilled.com>
2025-04-21 00:12:36 -0500

    updated test for localhost

--- tests/bagel-shop/test.spec.ts
+++ tests/bagel-shop/test.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from "@playwright/test";
 
 test("Bagel Shop Smoke Test", async ({ page }) => {
-  await page.goto("http://localhost:5173/");
+  await page.goto("http://localhost:5173");
 
   await expect(
     page.getByText("The best bagels in town since 1995!")
