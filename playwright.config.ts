2dfce254ef053a3f5770852432225ec0e239a076
Yash Tekade <128163324+yashtekade21@users.noreply.github.com>
2025-07-04 07:02:42 +0000

    installed playwright and typescript

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,86 +1,63 @@
-import { defineConfig, devices, expect } from "@playwright/test";
+import { defineConfig, devices } from '@playwright/test';
 
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-import dotenv from "dotenv";
-import path from "path";
-dotenv.config({ path: path.resolve(__dirname, ".env") });
+// import dotenv from 'dotenv';
+// import path from 'path';
+// dotenv.config({ path: path.resolve(__dirname, '.env') });
 
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  webServer: {
-    command: "npm run start",
-    port: 5173,
-    timeout: 120 * 1000,
-    reuseExistingServer: !process.env.CI,
-  },
-  timeout: 30_000,
-  globalTimeout: 10 * 60 * 1000,
-  testDir: "./tests",
+  testDir: './e2e',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  /* Retry on CI 2 times and locally 1 time */
-  retries: process.env.CI ? 2 : 1,
+  /* Retry on CI only */
+  retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [["html"], ["list"]],
+  reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: process.env.URL,
-    testIdAttribute: "data-test",
+    // baseURL: 'http://localhost:3000',
+
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: "on",
-    actionTimeout: 0,
-    ignoreHTTPSErrors: true,
-    video: "retain-on-failure",
-    screenshot: "only-on-failure",
-    headless: true,
+    trace: 'on-first-retry',
   },
 
   /* Configure projects for major browsers */
   projects: [
     {
-      name: "setup",
-      testMatch: /.*\.setup\.ts/,
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
     },
-    {
-      name: "chromium",
-      dependencies: ["setup"],
-      use: {
-        ...devices["Desktop Chrome"],
-        // viewport: { width: 1280, height: 720 },
-        permissions: ["clipboard-read", "geolocation"],
-      },
-    },
-    // {
-    //   name: 'firefox',
-    //   dependencies: ["setup"],
-    //   use: { ...devices['Desktop Firefox'] },
-    // },
 
-    // {
-    //   name: 'webkit',
-    //   dependencies: ["setup"],
-    //   use: { ...devices['Desktop Safari'] },
-    // },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
 
     /* Test against mobile viewports. */
     // {
     //   name: 'Mobile Chrome',
     //   use: { ...devices['Pixel 5'] },
     // },
-    {
-      name: "Mobile Safari",
-      use: { ...devices["iPhone 13"] },
-    },
+    // {
+    //   name: 'Mobile Safari',
+    //   use: { ...devices['iPhone 12'] },
+    // },
 
     /* Test against branded browsers. */
     // {
@@ -96,26 +73,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   // webServer: {
   //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
+  //   url: 'http://localhost:3000',
   //   reuseExistingServer: !process.env.CI,
   // },
-});
-
-expect.extend({
-  toBeNumber(received: number) {
-    const check = typeof received == "number";
-
-    if (check) {
-      return {
-        message: () => "passed",
-        pass: true,
-      };
-    } else {
-      return {
-        message: () =>
-          `toBeNumber() assertion failed.\nYou expected '${received}' to be a number but it's a ${typeof received}\n`,
-        pass: false,
-      };
-    }
-  },
 });
